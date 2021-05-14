f32b0ce1fbf61b2c154d40f6c4e41dbf81bb4fdb
fuzuosheng <szf3@qq.com>
2021-05-14 15:51:51 +0800

    完成登录注册功能

--- pages/login.vue
+++ pages/login.vue
@@ -8,8 +8,8 @@
 				欢迎使用云课堂
 			</view>
 		</view>
-		<view class="login-view">
-			<view class="t-login">
+		<view class="plane-view" v-if="isLoginView">
+			<view class="t-plane">
 				<form class="cl">
 					<view class="t-a">
 						<text class="txt">用户名</text>
@@ -20,21 +20,25 @@
 						<input type="password" name="code" maxlength="18" placeholder="请输入您的密码" v-model="pwd" />
 					</view>
 					<button @tap="login()">登 录</button>
-					<view class="reg" @tap="reg()">注 册</view>
+					<view class="reg" @tap="showRegistView()">注 册</view>
 				</form>
 			</view>
 		</view>
 		
-		<view class="login-view" style="display: none;">
-			<view class="t-login">
+		<view v-else class="plane-view">
+			<view class="t-plane">
 				<form class="cl">
 					<view class="t-a">
 						<text class="txt">用户名</text>
-						<input type="text" name="user" placeholder="请输入您的用户名" maxlength="11" v-model="user" />
+						<input type="text" name="user" placeholder="请输入用户名" maxlength="11" v-model="user" />
 					</view>
 					<view class="t-a">
 						<text class="txt">密码</text>
 						<input type="password" name="code" maxlength="18" placeholder="请输入您的密码" v-model="pwd" />
+					</view>
+					<view class="t-a">
+						<text class="txt">确认密码</text>
+						<input type="password" name="code" maxlength="18" placeholder="请再次输入您的密码" v-model="pwd_c" />
 					</view>
 					<view class="t-a">
 						<text class="txt">手机</text>
@@ -46,14 +50,14 @@
 					</view>
 	
 					<view class="t-a">
-						<text class="txt">是否教师</text>
-						<radio-group name="lecturer">
-							<radio name="lecturer" v-model="lecturer">是</radio>
-							<radio name="lecturer" v-model="lecturer">否</radio>
+						<text class="txt">身份</text>
+						<radio-group name="lecturer" v-model="lecturer" @change="registRadioValueChange">
+							<radio value="true">教师</radio>
+							<radio value="false" checked="true">学生</radio>
 						</radio-group>
-
 					</view>
 					<button class="reg" @tap="reg()">注 册</button>
+					<text @click="showLoginView()" >已有账号 去登录</text>
 				</form>
 			</view>
 		</view>
@@ -63,12 +67,20 @@
 export default {
 	data() {
 		return {
+			user:'',
+			pwd_c:'',
+			email:'',
 			phone: '', //手机号码
-			pwd: '' //密码
+			pwd: '', //密码
+			lecturer:'',
+			isLoginView:true
 		};
 	},
 	onLoad() {},
 	methods: {
+		registRadioValueChange(evt){
+			this.lecturer=evt.detail.value;
+		},
 		//当前登录按钮操作
 		login() {
 			var that = this;
@@ -102,19 +114,104 @@ export default {
 						url:"user"
 					});
 				}else{
-					
+					uni.showToast({
+						title:'账号或密码不正确',
+						icon:'none'
+					});
 				}
 			});
 			
 		},
+		showRegistView(){
+			this.isLoginView=false;
+		},
+		showLoginView(){
+			this.isLoginView=true;
+		},
 		//注册按钮点击
 		reg() {
-			uni.showToast({ title: '注册跳转', icon: 'none' });
+			if(!/^[\d\w]{4,}$/.test(this.user)){
+				uni.showToast({ title: '请输入用户名', icon: 'none' });
+				return;
+			}
+			
+			if(this.pwd.length<6){
+				uni.showToast({
+					title:'密码长度至少为6位,可包含英文字母和数字',
+					icon:'none'
+				});
+				return;
+			}
+			if(!/^[\d\w]{6,}$/.test(this.pwd)){
+				uni.showToast({
+					title:'密码中只能包含英文字母和数字',
+					icon:'none'
+				});
+				return;
+			}
+			if(this.pwd!==this.pwd_c){
+				uni.showToast({
+					title:'两次输入的密码不一致',
+					icon:'none'
+				});
+				return;
+			}
+			if(!/^1[3-9]\d{9}$/.test(this.phone)){
+				uni.showToast({
+					title:'请输入正确的手机号',
+					icon:'none'
+				});
+				return;
+			}
+			if(!/^([a-zA-Z0-9]+[_|_|\-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/.test(this.email)){
+				uni.showToast({
+					title:'请输入正确的邮箱地址',
+					icon:'none'
+				});
+				return;
+			}
+			
+			console.log('用户名',this.user);
+			console.log('密码',this.pwd);
+			console.log('手机号',this.phone);
+			console.log('邮箱地址',this.email);
+			console.log('教师身份？',this.lecturer);
+			this.$requestData({
+				url:'/auth/regist',
+				data:{
+					userName:this.user,
+					password:this.pwd,
+					phoneNumber:this.phone,
+					email:this.email,
+					lecturer:this.lecturer
+				}
+			}).then((res)=>{
+				if(res.data.code==200){
+					uni.showToast({
+						title:'注册成功!',
+						icon:'success'
+					});
+					this.isLoginView=true;
+					//自动登录
+					
+				}else{
+					if(res.data.code==1)
+					uni.showToast({
+						title:'用户名已存在',
+						icon:'none'
+					})
+				}
+			});
+			
 		}
 	}
 };
 </script>
 <style>
+	
+body{
+	background: #FFF;
+}
 .txt {
 	font-size: 32rpx;
 	font-weight: bold;
@@ -139,22 +236,23 @@ export default {
 	margin-top: 30rpx;
 }
 
-.login-view {
+.plane-view {
 	width: 100%;
 	position: relative;
 	margin-top: -120rpx;
 	background-color: #ffffff;
-	border-radius: 8% 8% 0% 0;
+	border-radius: 8% 8% 8% 8%;
+	padding: 10rpx 0rpx 80rpx 0rpx;
 }
 
-.t-login {
+.t-plane {
 	width: 600rpx;
 	margin: 0 auto;
 	font-size: 28rpx;
 	padding-top: 80rpx;
 }
 
-.t-login button {
+.t-plane button {
 	font-size: 28rpx;
 	background: #57e;
 	color: #fff;
@@ -164,7 +262,7 @@ export default {
 	font-weight: bold;
 }
 
-.t-login input {
+.t-plane input {
 	height: 90rpx;
 	line-height: 90rpx;
 	margin-bottom: 50rpx;
@@ -172,7 +270,7 @@ export default {
 	font-size: 28rpx;
 }
 
-.t-login .t-a {
+.t-plane .t-a {
 	position: relative;
 }
 
@@ -185,7 +283,7 @@ export default {
 	line-height: 70rpx;
 }
 
-.t-login .t-c {
+.t-plane .t-c {
 	position: absolute;
 	right: 22rpx;
 	top: 22rpx;
@@ -198,41 +296,41 @@ export default {
 	padding: 0 25rpx;
 }
 
-.t-login .t-d {
+.t-plane .t-d {
 	text-align: center;
 	color: #999;
 	margin: 80rpx 0;
 }
 
-.t-login .t-e {
+.t-plane .t-e {
 	text-align: center;
 	width: 250rpx;
 	margin: 80rpx auto 0;
 }
 
-.t-login .t-g {
+.t-plane .t-g {
 	float: left;
 	width: 50%;
 }
 
-.t-login .t-e image {
+.t-plane .t-e image {
 	width: 50rpx;
 	height: 50rpx;
 }
 
-.t-login .t-f {
+.t-plane .t-f {
 	text-align: center;
 	margin: 150rpx 0 0 0;
 	color: #666;
 }
 
-.t-login .t-f text {
+.t-plane .t-f text {
 	margin-left: 20rpx;
 	color: #aaaaaa;
 	font-size: 27rpx;
 }
 
-.t-login .uni-input-placeholder {
+.t-plane .uni-input-placeholder {
 	color: #aeaeae;
 }
 
