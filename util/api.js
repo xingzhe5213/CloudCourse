78d1c34f59de2102a4aae6d17d735d27c4bc62d8
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2021-05-15 13:36:08 +0800

    接口封装修改

--- util/api.js
+++ util/api.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 
 export const requestData=(options)=>{ //传入的options是一个json对象
 	let headers={}
-	// var token =uni.getStorageSync('token');
-	// headers['token'] = token;
+	var token =uni.getStorageSync('token');
 	
-	headers['token'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IjEyMzQ1NiIsInJvbGUiOiJmYWxzZSIsInVzZXJOYW1lIjoiYWRtaW4iLCJleHAiOjE2MjMwNTA3OTJ9.pclB51L2Z3RcvfSdEyptrNmFJ087mvmh7488XbRTvd4";
+	headers['token'] = token;
 	headers['Content-Type'] = "application/x-www-form-urlencoded";
 	
 	return new Promise((resolve,reject)=>{
@@ -17,11 +16,6 @@ export const requestData=(options)=>{ //传入的options是一个json对象
 			dataType:options.dataType || "json",
 			success: (res) => {
 				// console.log(res)
-				if(res.statusCode !== 200){
-					return uni.showToast({
-						title:"系统异常！"
-					})
-				}
 				resolve(res)
 			},
 			fail: (err) => {
